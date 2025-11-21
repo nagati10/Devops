45031d0ba8bd8821e6f2d2099af5d6604f5a5da6
nagati10 <130963507+nagati10@users.noreply.github.com>
2025-11-21 23:00:39 +0100

    Update StudentManagementApplicationTests.java

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -2,8 +2,6 @@ package tn.esprit.studentmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
-
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.properties")
 class StudentManagementApplicationTests {
